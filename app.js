e893aa581a025553839144aca873ad1d626e4ffa
JulioSantos <jcherilli@gmail.com>
2024-08-21 23:14:47 -0300

    teste novo

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ function sortear() {
     let numeroFim = document.getElementById('ate').value;
     alert(`Quantidade de números: ${quantidade}`);
     alert(`Do número: ${de}`); alert(`Até: ${ate}`);
+    
 }
