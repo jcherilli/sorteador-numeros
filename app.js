17fc4160730a46c2fd5bd3621e0374c38233b1d5
JulioSantos <jcherilli@gmail.com>
2024-08-21 23:06:56 -0300

    teste

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let numeroInicio = document.getElementById('de').value;
+    let numeroFim = document.getElementById('ate').value;
+    alert(`Quantidade de números: ${quantidade}`);
+    alert(`Do número: ${de}`); alert(`Até: ${ate}`);
+}
